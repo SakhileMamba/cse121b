e76f8259e5ddd3d2f6cfd50fb9a168456953463d
Sakhile <sakhymamba@gmail.com>
2026-08-13 02:12:13 +0200

    Fix formatting of API key in project.js

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -5,7 +5,7 @@ async function fetchMovies() {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '1fd22aa7damsh9d9ad651cac54b2p127909jsn689266f1610c'',
+            'X-RapidAPI-Key': '1fd22aa7damsh9d9ad651cac54b2p127909jsn689266f1610c',
             'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
         }
     };
